f3ca27e8ffd9bff66fff1078f5251b8ea036e3e7
multazamalaih <multazam071220@gmail.com>
2025-04-25 20:24:41 +0700

    Add migration users

--- app/Database/Migrations/2025-04-25-130956_Users.php
+++ app/Database/Migrations/2025-04-25-130956_Users.php
@@ -8,11 +8,54 @@ class Users extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            "id_user" => [
+                "type" => "INT",
+                "auto_increment" => true,
+                "null" => true,
+                "constraint" => 11,
+            ],
+            "nama" => [
+                "type" => "VARCHAR",
+                "null" => false,
+                "constraint" => 100,
+            ],
+            "email" => [
+                "type" => "VARCHAR",
+                "null" => false,
+                "constraint" => 100,
+            ],
+            "password" => [
+                "type" => "VARCHAR",
+                "null" => false,
+                "constraint" => 255,
+            ],
+            "role" => [
+                "type" => "ENUM",
+                "constraint" => ["Admin", "User"],
+                "default" => "User",
+            ],
+            "created_at" => [
+                "type" => "DATETIME",
+                "null" => true,
+            ],
+            "updated_at" => [
+                "type" => "DATETIME",
+                "null" => true,
+            ],
+            "deleted_at" => [
+                "type" => "DATETIME",
+                "null" => true,
+            ],
+        ]);
+        $this->forge->addKey("id_user", true);
+        $this->forge->addUniqueKey("nama");
+        $this->forge->addUniqueKey("email");
+        $this->forge->createTable("users");
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable("users");
     }
 }
